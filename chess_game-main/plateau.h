eb2a9866c061041e897d15e8eb8f940ba0ac6f7f
redabenjelloun357 <r.benjelloun@esisa.ac.ma>
2026-04-04 12:20:25 +0200

    Add files via upload

---
+++ chess_game-main/plateau.h
@@ -0,0 +1,23 @@
+#ifndef PLATEAU_H
+#define PLATEAU_H
+
+class plateau
+{
+	public:
+		plateau();
+		~plateau();
+		init_Plateau();
+		int get_Piece(int i,int j);
+		mov_Piece(int ld,int cd,int la,int ca);
+		int check_arival_space(int ld,int cd,int la,int ca);
+		int evaluation(int ld,int cd,int la,int ca);
+		int eval_pion(int ld,int cd,int la,int ca);
+		int eval_cavalier(int ld,int cd,int la,int ca);
+		int eval_tour(int ld,int cd,int la,int ca);
+		int eval_fou(int ld,int cd,int la,int ca);
+		
+	private:
+		int ech[8][8];
+};
+
+#endif
